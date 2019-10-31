33f790d730d5cc4a3c6e645d3e2a302fcb5839dc
leejoo71 <leejoo71@gmail.com>
2019-11-01 01:58:54 +0800

    Update build_openalpr_android.sh

--- build_openalpr_android.sh
+++ build_openalpr_android.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 # You should tweak this section to adapt the paths to your need
-export ANDROID_HOME=/usr/lib/android-sdk
-export NDK_ROOT=/usr/lib/android-sdk/ndk-bundle
+#export ANDROID_HOME=/usr/lib/android-sdk
+#export NDK_ROOT=/usr/lib/android-sdk/ndk-bundle
+if [ -d "$HOME/Android/Sdk" ]; then
+  export ANDROID_HOME=$HOME/Android/Sdk
+  if [ -d "$ANDROID_HOME/ndk_bundle" ]; then
+    export NDK_ROOT=$ANDROID_HOME/ndk_bundle
+    export ANDROID_NDK_HOME=$NDK_ROOT
+  fi
+  PATH=$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin
+fi
+echo $HOME
+echo $ANDROID_HOME
+echo $NDK_ROOT
 
 ANDROID_PLATFORM="android-28"
 
