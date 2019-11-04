98de235c03bbbf52f94dc93c4b272f41a22d34f8
leejoo71 <leejoo71@gmail.com>
2019-11-04 23:21:28 +0800

    Update build_openalpr_android.sh

--- build_openalpr_android.sh
+++ build_openalpr_android.sh
@@ -118,9 +118,10 @@ Generating project for arch $i
 	-DANDROID_ABI="$i" \
 	-DANDROID_CPP_FEATURES="rtti exceptions" \
 	-DANDROID_STL=c++_static \
-	-DANDROID_COMPILER_FLAGS=-lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_shape -lopencv_videoio -lopencv_calib3d -lopencv_fac \
+	-DANDROID_COMPILER_FLAGS=-lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_shape -lopencv_videoio -lopencv_calib3d \
 	-DANDROID_LINKER_FLAGS=-lopencv_core
 	-DANDROID_ARM_MODE=arm \
+	-DANDROID_LD= -latomic \
 	
 	-DTesseract_INCLUDE_BASEAPI_DIR=$TESSERACT_SRC_DIR/api \
 	-DTesseract_INCLUDE_CCSTRUCT_DIR=$TESSERACT_SRC_DIR/ccstruct \
